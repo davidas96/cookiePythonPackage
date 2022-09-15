f13bcf9adbeec785776afa4dff3219e5ac6cffcc
david.dujakovic <david.dujakovic@factoryww.com>
2022-09-15 10:20:21 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+def saberi_dva_broja(broj1: int = 1, broj2: int = 2) -> int:
+    return broj1 + broj2
+
---
+++ test_sabiranje.py
@@ -0,0 +1,6 @@
+from main import saberi_dva_broja
+
+
+def test_saberi_dva_broja():
+    rezultat = saberi_dva_broja(2,3)
+    assert rezultat == 5
